d2301363291bd43a9e9af41ebe1d0c3d8b60af7a
Foparty <alfonsomartingarcia@gmail.com>
2023-09-02 21:52:31 +0200

    priority image

--- components/MyImageContainer/MyImageContainer.js
+++ components/MyImageContainer/MyImageContainer.js
@@ -1,14 +1,7 @@
-import styles from "./MyImageContainer.module.css";
-import Image from "next/image";
+import styles from './MyImageContainer.module.css';
+import Image from 'next/image';
 
 const MyImageContainer = ({ altText, object, img }) => {
-  return (
-    <Image
-      className={styles.img}
-      style={{ objectFit: object }}
-      src={img}
-      alt={`image of ${altText}`}
-    />
-  );
+	return <Image className={styles.img} style={{ objectFit: object }} src={img} alt={`image of ${altText}`} priority={true} />;
 };
 export default MyImageContainer;
